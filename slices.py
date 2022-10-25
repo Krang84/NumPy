76aba8ec20880783213527e67b2b1be632cc13c8
Krang84 <patelassad@gmail.com>
2022-10-25 18:53:00 +0200

    Create slices.py

---
+++ slices.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# 1 dimensionnal array
+arr=np.arange(10)
+
+arr[:2]
+arr[:]
+
+
+arr2d=np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+#first two rows
+arr[:2] 
+
+first two columns
+arr[:, :2]
+
+
