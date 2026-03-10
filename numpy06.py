4ab0e49e2cbb830775a7b69e87fbd88644fe8458
Krang84 <patelassad@gmail.com>
2026-03-10 11:24:24 +0100

    Create numpy06.py

---
+++ numpy06.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# input : two lists
+list1=[1,2,3,4,5]
+list2=[6,7,8,9,10]
+
+# fonction array([liste, liste])
+
+r2 = np.array([list1, list2])
+
+print(r2)
+
+# output : two dimensionnal array
+#[[ 1  2  3  4  5]
+# [ 6  7  8  9 10]]
